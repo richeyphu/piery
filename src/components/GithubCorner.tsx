9a9f31bb195e2c025a083b6b5247c9b3ef4912fc
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2022-10-19 21:35:15 +0700

    feat: github corner color mode

--- src/components/GithubCorner.tsx
+++ src/components/GithubCorner.tsx
@@ -1,12 +1,16 @@
 import GithubCorner from "react-github-corner";
-import { Box } from "@chakra-ui/react";
+import { Box, useColorMode } from "@chakra-ui/react";
 
 type Props = {};
 
 const _GithubCorner = (props: Props) => {
   return (
     <Box pos="fixed" top={0} right={0}>
-      <GithubCorner href="https://github.com/richeyphu/piery" />
+      <GithubCorner
+        href="https://github.com/richeyphu/piery"
+        octoColor={useColorMode().colorMode === "dark" ? "#151513" : "#fff"}
+        bannerColor={useColorMode().colorMode === "dark" ? "#fff" : "#151513"}
+      />
     </Box>
   );
 };
