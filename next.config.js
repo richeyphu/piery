5a672d8e98be6a0aec792759fe8623e8b4cdf39e
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2022-10-20 01:30:06 +0700

    Install Preact for making app faster (ngl)

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const withPreact = require("next-plugin-preact");
+
+const nextConfig = withPreact({
   reactStrictMode: true,
   swcMinify: true,
-}
+});
 
-module.exports = nextConfig
+module.exports = nextConfig;
