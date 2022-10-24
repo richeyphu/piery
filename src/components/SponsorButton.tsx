0a919bfb2446ceecebaef1e80ef5a8804487268c
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2022-10-24 17:10:41 +0700

    Create sponsor button

---
+++ src/components/SponsorButton.tsx
@@ -0,0 +1,14 @@
+const SponsorButton = () => {
+  return (
+    <iframe
+      src="https://github.com/sponsors/richeyphu/button?transparent=0"
+      title="Sponsor richeyphu"
+      height="32"
+      width="116"
+      style={{ border: 0, borderRadius: "7px" }}
+      allowTransparency={true}
+    ></iframe>
+  );
+};
+
+export default SponsorButton;
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as ColorToggle } from "./ColorToggle";
 export { default as GithubCorner } from "./GithubCorner";
 export { default as ScrollToTop } from "./ScrollToTop";
 export { default as MetaTags } from "./MetaTags";
+export { default as SponsorButton } from "./SponsorButton";
