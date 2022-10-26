0ca7886ea4adbce317db3f15b5df79771893d170
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2022-10-26 20:01:34 +0700

    Add manifest tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,6 +12,8 @@ export default class Document extends NextDocument {
             rel="icon"
             href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🥧</text></svg>"
           />
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="apple-touch-icon" href="/icon-192x192.png"></link>
           <MetaTags />
         </Head>
         <body>
