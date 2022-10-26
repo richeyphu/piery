189bba53489836c63d1b4710b7d8ac482f218efc
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2022-10-26 20:15:20 +0700

    Config PWA

--- next.config.js
+++ next.config.js
@@ -1,9 +1,17 @@
-/** @type {import('next').NextConfig} */
 const withPreact = require("next-plugin-preact");
-
-const nextConfig = withPreact({
-  reactStrictMode: true,
-  swcMinify: true,
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
 });
+
+/** @type {import('next').NextConfig} */
+const nextConfig = withPWA(
+  withPreact({
+    reactStrictMode: true,
+    swcMinify: true,
+  })
+);
 
 module.exports = nextConfig;
