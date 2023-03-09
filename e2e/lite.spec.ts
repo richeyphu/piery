3e3fbbc27822cad20c71c8d0733ce9660f78f86b
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2023-03-09 20:45:08 +0700

    Fix test step

---
+++ e2e/lite.spec.ts
@@ -0,0 +1,29 @@
+import { test, expect } from "@playwright/test";
+
+import { bakePiSteps, getPiByDigits } from "./oven.spec";
+
+test.describe("PI Oven Lite", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/lite");
+  });
+
+  test("should bake the same result as `Math.PI`", async ({ page }) => {
+    await bakePiSteps(
+      page,
+      Math.PI.toString().slice(2).length,
+      Math.PI.toString()
+    );
+  });
+
+  test("should bake 100 digits of PI", async ({ page }) => {
+    await bakePiSteps(page, 100, getPiByDigits(100));
+  });
+
+  test("should bake 1000 digits of PI", async ({ page }) => {
+    await bakePiSteps(page, 1000, getPiByDigits(1000));
+  });
+
+  test("should bake 10000 digits of PI", async ({ page }) => {
+    await bakePiSteps(page, 10000, getPiByDigits(10000));
+  });
+});
