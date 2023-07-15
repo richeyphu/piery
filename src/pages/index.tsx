187dbb56ce3c79a3534fbef88a30a856ab779701
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2023-07-15 07:23:01 +0700

    Disable countapi

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -146,7 +146,7 @@ const Home: NextPage = () => {
           name="theme-color"
           content={useColorMode().colorMode === "dark" ? "#1a202c" : "#fff"}
         />
-        <script async src="https://api.countapi.xyz/hit/piery-web/visits" />
+        {/* <script async src="https://api.countapi.xyz/hit/piery-web/visits" /> */}
       </Head>
 
       {/* 
